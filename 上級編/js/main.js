21d1e501f4c067fb31cfb149ea3202aa9de25e19
wilas0o0o <03mcu30@gmail.com>
2022-05-13 17:21:42 +0900

    [Add]slick

--- 上級編/js/main.js
+++ 上級編/js/main.js
@@ -2,4 +2,25 @@ $(function() {
   $('.toggle-btn').on('click', function() {
     $('#header').toggleClass('open');
   });
+
+  $('.mask').on('click', function() {
+    ('#header').removeClass('open');
+  });
+
+  $('.slick-area').slick({
+    arrows: false,
+    autoplay: true,
+    centerMode: true,
+    centerPadding: '100px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakPoint: 768,
+        settings: {
+          centerPadding: '50px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
 });
